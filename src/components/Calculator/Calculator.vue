ff4ebfa7915dcb255b3bb129e91d950bf7966fc0
victormedeiros1 <josevictordev@gmail.com>
2024-12-01 18:10:55 -0300

    feat: component Divider

--- src/components/Calculator/Calculator.vue
+++ src/components/Calculator/Calculator.vue
@@ -3,11 +3,13 @@
     <h1>Calculadora de investimentos</h1>
     <Form />
     <Button>CALCULAR</Button>
+    <Divider />
   </div>
 </template>
 
 <script setup lang="ts">
   import Button from '@/components/Button/Button.vue'
+  import Divider from '@/components/Divider/Divider.vue'
   import Form from '@/components/Form/Form.vue'
 </script>
 
---
+++ src/components/Divider/Divider.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="divider"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+  .divider {
+    width: 100%;
+    max-width: 600px;
+    height: 1px;
+    background-color: $primary--main;
+  }
+</style>
